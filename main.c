1235af3cf5804b17d57d4b99d49154e0f17c12b9
charlessoft <charlessoft@qq.com>
2014-10-19 21:14:02 +0800

    delete temp

--- main.c
+++ main.c
@@ -86,7 +86,6 @@ int get_file( const char* path )
 
 int post_file( const char* path )
 {
-    //string strUrl = "http://10.142.49.238:9080/udsfs/uploadfile";
     string postFileData;
     string strBoundary = "----WebKitFormBoundaryx386qjAs2dAgshUG";
     string formMultipart = "Content-Type: multipart/form-data; boundary=" + strBoundary;
@@ -124,9 +123,6 @@ int post_file( const char* path )
     if ( nres ==0 && nLen>0 ) {
         return nLen;
     }
-    //string strrespbuf = CService.m_resp_buffer;
-    //string strrespheader = CService.m_resp_header;
-    //printf("strrespbuf=%s\n",strrespbuf.c_str());
 }
 
 void *mfs_init(struct fuse_conn_info *conn)
@@ -138,19 +134,14 @@ void *mfs_init(struct fuse_conn_info *conn)
 
 static int mfs_getattr(const char *path, struct stat *stbuf)
 {
-    log_msg("\nmfs_getattr\n    path=%s\n",path);
-    //get_file(path);
-    log_stat(stbuf);
-    //if(strcmp(path, "/") ==0 )
-        //stbuf->st_mode = 0755 | S_IFDIR;
-    //else 
-        //stbuf->st_mode = 0755 | S_IFREG;
-
-        //return 0;
+	log_msg("\nmfs_getattr\n    path=%s\n",path);
+	//get_file(path);
+	log_stat(stbuf);
+	//return 0;
 	int res = 0;
 	res = lstat(path, stbuf);
-    log_msg("\nmfs_getattr--lstat()\n    res=%d",res);
-    log_stat(stbuf);
+	log_msg("\nmfs_getattr--lstat()\n    res=%d",res);
+	log_stat(stbuf);
 	if (res == -1)
 		return -errno;
 
@@ -194,42 +185,42 @@ static int mfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     dp = opendir(path);
     //printf("path===%s\n",path);
     //dp = opendir("/home/charles");
-	if (dp == NULL)
-		return -errno;
+    if (dp == NULL)
+        return -errno;
 
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
-	}
+    while ((de = readdir(dp)) != NULL) {
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        st.st_ino = de->d_ino;
+        st.st_mode = de->d_type << 12;
+        if (filler(buf, de->d_name, &st, 0))
+            break;
+    }
 
-	closedir(dp);
-	return 0;
+    closedir(dp);
+    return 0;
 }
 
 
 static int mfs_mkdir(const char *path, mode_t mode)
 {
-	int res =0 ;
-	res = mkdir(path, mode);
-	if (res == -1)
-		return -errno;
+    int res =0 ;
+    res = mkdir(path, mode);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 
 static int mfs_rmdir(const char *path)
 {
-	int res =0;
-	res = rmdir(path);
-	if (res == -1)
-		return -errno;
+    int res =0;
+    res = rmdir(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 
@@ -237,13 +228,13 @@ static int mfs_rmdir(const char *path)
 static int mfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
 {
     log_msg("\nmfs_create()\n    path=%s\n    mode=%d",path,mode);
-	int fd = 0;
-	fd = open(path, fi->flags, mode);
-	if (fd == -1)
-		return -errno;
+    int fd = 0;
+    fd = open(path, fi->flags, mode);
+    if (fd == -1)
+        return -errno;
 
-	fi->fh = fd;
-	return 0;
+    fi->fh = fd;
+    return 0;
 
 
     //int fd = 0;
@@ -251,7 +242,7 @@ static int mfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
     //log_fi(fi);
     //fd = open( path, fi->flags, mode );
     //if( fd == -1 )
-        //return -errno;
+    //return -errno;
     //fi->fh = fd;
     //return 0;
 }
@@ -259,29 +250,29 @@ static int mfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
 static int mfs_unlink(const char *path)
 {
     log_msg("\nmfs_unlink()\n    path=%s\n",path);
-	int res = 0;
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
+    int res = 0;
+    res = unlink(path);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 
 static int mfs_open(const char *path, struct fuse_file_info *fi)
 {
-	int res = 0;
-	res = open(path, fi->flags);
+    int res = 0;
+    res = open(path, fi->flags);
     log_msg("\nmfs_open()\n    path=%s\nres=%d\n",path,res);
-	if (res == -1)
-		return -errno;
+    if (res == -1)
+        return -errno;
 
-	close(res);
-	return 0;
+    close(res);
+    return 0;
 }
 
 static int mfs_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
+                     off_t offset, struct fuse_file_info *fi)
 {
     log_msg("\nmfs_write()\n    path=%s\n    buf=0x%08x\n    size=%d\n    offset=%d\n",path,buf,size,offset);
     //int fd;
@@ -299,7 +290,7 @@ static int mfs_write(const char *path, const char *buf, size_t size,
             g_cache.push_back(path+1);
     }
     return res;
-    
+
 
 }
 
@@ -308,29 +299,29 @@ static int mfs_write(const char *path, const char *buf, size_t size,
 static int mfs_utimens(const char *path, const struct timespec ts[2])
 {
     log_msg("\nmfs_utimens()\n    path=%s\n");
-	int res;
-	struct timeval tv[2];
+    int res;
+    struct timeval tv[2];
 
-	tv[0].tv_sec = ts[0].tv_sec;
-	tv[0].tv_usec = ts[0].tv_nsec / 1000;
-	tv[1].tv_sec = ts[1].tv_sec;
-	tv[1].tv_usec = ts[1].tv_nsec / 1000;
+    tv[0].tv_sec = ts[0].tv_sec;
+    tv[0].tv_usec = ts[0].tv_nsec / 1000;
+    tv[1].tv_sec = ts[1].tv_sec;
+    tv[1].tv_usec = ts[1].tv_nsec / 1000;
 
-	res = utimes(path, tv);
-	if (res == -1)
-		return -errno;
+    res = utimes(path, tv);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
 static int mfs_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
+                    struct fuse_file_info *fi)
 {
     log_msg("\n----mfs_read()\n    path=%s\n    buf=0x%08x\n    size=%d\n    offset=%d\n",
             path,buf,size,offset);
     log_fi(fi);
     //int fd;
-	int res = 0;
+    int res = 0;
 
     char szfileurl[256] = {0};
     strcpy(szfileurl,BB_DATA->httpgeturl);
@@ -339,109 +330,109 @@ static int mfs_read(const char *path, char *buf, size_t size, off_t offset,
     log_msg("\n------download url = %s\n", szfileurl );
 
     //curl_easy_setopt(easy_handle, CURLOPT_URL, szfileurl);
-    
-	//(void) fi;
-	//fd = open(path, O_RDONLY);
-	//if (fd == -1)
-		//return -errno;
 
-	//res = pread(fd, buf, size, offset);
-	//if (res == -1)
-		//res = -errno;
+    //(void) fi;
+    //fd = open(path, O_RDONLY);
+    //if (fd == -1)
+    //return -errno;
 
-	//close(fd);
-	return res;
+    //res = pread(fd, buf, size, offset);
+    //if (res == -1)
+    //res = -errno;
+
+    //close(fd);
+    return res;
 }
 
 static int mfs_truncate(const char *path, off_t size)
 {
-	int res;
+    int res;
 
-	res = truncate(path, size);
-	if (res == -1)
-		return -errno;
+    res = truncate(path, size);
+    if (res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
- static struct vsfuse_oper : fuse_operations  {
-     vsfuse_oper()
-     {   
-         //readdir = bb_readdir;
-         //getattr = bb_getattr;
-         //init = bb_init;
-         //destroy = bb_destroy;
-         ////opdir = xmp_opendir;
+static struct vsfuse_oper : fuse_operations  {
+    vsfuse_oper()
+    {   
+        //readdir = bb_readdir;
+        //getattr = bb_getattr;
+        //init = bb_init;
+        //destroy = bb_destroy;
+        ////opdir = xmp_opendir;
 
-         init       = mfs_init;
-         //目录操作
-         getattr	= mfs_getattr;
-         readdir	= mfs_readdir;
-         mkdir		= mfs_mkdir;
-         rmdir		= mfs_rmdir;
+        init       = mfs_init;
+        //目录操作
+        getattr	= mfs_getattr;
+        readdir	= mfs_readdir;
+        mkdir		= mfs_mkdir;
+        rmdir		= mfs_rmdir;
 
-         //创建文件操作回调
-         create		= mfs_create; 
-         open		= mfs_open;
-         utimens	= mfs_utimens;
+        //创建文件操作回调
+        create		= mfs_create; 
+        open		= mfs_open;
+        utimens	= mfs_utimens;
 
-         //删除文件
-         unlink		= mfs_unlink;
+        //删除文件
+        unlink		= mfs_unlink;
 
-         //文件读写回调
-         read		= mfs_read;
-         write		= mfs_write;
+        //文件读写回调
+        read		= mfs_read;
+        write		= mfs_write;
 
-                    //rename		= mfs_rename,
-         truncate	= mfs_truncate;
-    //opendir    = mfs_opendir;
-    //access		= mfs_access;
-    //readlink	= mfs_readlink;
-    //mknod		= mfs_mknod;
-    //symlink	= mfs_symlink;
-    //link		= mfs_link;
-    //chmod		= mfs_chmod;
-    //chown		= mfs_chown;
-    //statfs		= mfs_statfs;
-    //release	= mfs_release;
-    //fsync		= mfs_fsync;
- 
-     }   
- }mfs_oper;
+        //rename		= mfs_rename,
+        truncate	= mfs_truncate;
+        //opendir    = mfs_opendir;
+        //access		= mfs_access;
+        //readlink	= mfs_readlink;
+        //mknod		= mfs_mknod;
+        //symlink	= mfs_symlink;
+        //link		= mfs_link;
+        //chmod		= mfs_chmod;
+        //chown		= mfs_chown;
+        //statfs		= mfs_statfs;
+        //release	= mfs_release;
+        //fsync		= mfs_fsync;
+
+    }   
+}mfs_oper;
 
 //static struct fuse_operations mfs_oper = {
-    //.init       = mfs_init,
-    ////目录操作
-	//.getattr	= mfs_getattr,
-	//.readdir	= mfs_readdir,
-    //.mkdir		= mfs_mkdir,
-    //.rmdir		= mfs_rmdir,
-    
-    ////创建文件操作回调
-	//.create		= mfs_create, 
-    //.open		= mfs_open,
-    //.utimens	= mfs_utimens,
+//.init       = mfs_init,
+////目录操作
+//.getattr	= mfs_getattr,
+//.readdir	= mfs_readdir,
+//.mkdir		= mfs_mkdir,
+//.rmdir		= mfs_rmdir,
 
-    ////删除文件
-    //.unlink		= mfs_unlink, 
+////创建文件操作回调
+//.create		= mfs_create, 
+//.open		= mfs_open,
+//.utimens	= mfs_utimens,
 
-    ////文件读写回调
-    //.read		= mfs_read,
-    //.write		= mfs_write,
-    
-    ////.rename		= mfs_rename,
-    ////.truncate	= mfs_truncate,
-    ////.opendir    = mfs_opendir,
-    ////.access		= mfs_access,
-    ////.readlink	= mfs_readlink,
-    ////.mknod		= mfs_mknod,
-    ////.symlink	= mfs_symlink,
-    ////.link		= mfs_link,
-    ////.chmod		= mfs_chmod,
-    ////.chown		= mfs_chown,
-    ////.statfs		= mfs_statfs,
-    ////.release	= mfs_release,
-    ////.fsync		= mfs_fsync,
+////删除文件
+//.unlink		= mfs_unlink, 
+
+////文件读写回调
+//.read		= mfs_read,
+//.write		= mfs_write,
+
+////.rename		= mfs_rename,
+////.truncate	= mfs_truncate,
+////.opendir    = mfs_opendir,
+////.access		= mfs_access,
+////.readlink	= mfs_readlink,
+////.mknod		= mfs_mknod,
+////.symlink	= mfs_symlink,
+////.link		= mfs_link,
+////.chmod		= mfs_chmod,
+////.chown		= mfs_chown,
+////.statfs		= mfs_statfs,
+////.release	= mfs_release,
+////.fsync		= mfs_fsync,
 //};
 
 
@@ -498,40 +489,17 @@ int main(int argc, char *argv[])
     //curl_res = curl_global_init(CURL_GLOBAL_ALL);
     //if(curl_res != CURLE_OK )
     //{
-        //log_msg("\ncurl init fail\n");
-        //return -1;
+    //log_msg("\ncurl init fail\n");
+    //return -1;
     //}
 
 
-  
+
     bb_data = (bb_state*)malloc(sizeof(struct bb_state));
     memset(bb_data, 0, sizeof(bb_data));
     bb_data->logfile = log_open();
     bb_data->httpgeturl = "http://10.211.5.8/getdata.php?key=";
     bb_data->httpposturl = "http://10.142.49.238:9080/udsfs/uploadfile";
     //
-    //bb_data.curl_version = curl_version_info(CURLVERSION_NOW);
-    //bb_data.safe_nobody = ftpfs.curl_version->version_num > CURLFTPFS_BAD_NOBODY;
-    //bb_data.blksize = 4096;
-    //bb_data.disable_epsv = 1;
-    //bb_data.multiconn = 1;
-    //bb_data.attached_to_multi = 0;
-    ////bb_data.debug = 1;
-    
-
-    //easy = curl_easy_init();
-    //if(easy == NULL)
-    //{
-        //log_msg("\ncurl easy init fail\n");
-        //return -1;
-    //}
-    //else
-    //{
-        ////log_msg("\ncurl easy init ok\n");
-        //printf("curl easy init ok\n");
-    //}
-    //bb_data->connection = easy;
-    //int nres =  get_file_size("/tmp/10K.txt");
-    //printf("nsssss=%d\n",nres);
-	return fuse_main(argc, argv, &mfs_oper, bb_data);
+    return fuse_main(argc, argv, &mfs_oper, bb_data);
 }
